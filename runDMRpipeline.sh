b2efd8951468f2718d83f2c33ca6a5969284db2a
jildwild <jilke.dewilde@ugent.be>
2021-10-07 17:51:32 +0200

    new dir

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -1,6 +1,6 @@
 ml purge
 ml snakemake/6.1.0-foss-2020b
 
-snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRfinder.snakefile"
+snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 
 snakemake -s ${snakefile} --cluster "qsub -V" --default-resources --jobs 100 --rerun-incomplete
