ff7131ee247c12abcaabdd1704e5cbc0cb0ab553
jildwild <jilke.dewilde@ugent.be>
2021-10-09 17:14:22 +0200

    less jobs

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -3,4 +3,4 @@ ml snakemake/6.1.0-foss-2020b
 
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 
-snakemake -s ${snakefile} --cluster "qsub" --jobs 200 --rerun-incomplete
+snakemake -s ${snakefile} --cluster "qsub" --jobs 100 --rerun-incomplete
