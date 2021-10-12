439ae89fbeaebfdc1d151934b7f869cd86d41adf
jildwild <jilke.dewilde@ugent.be>
2021-10-12 12:53:28 +0200

    specify time because of oom kill

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -4,4 +4,4 @@ ml snakemake/6.1.0-foss-2020b
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 profile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/slurm_profile"
 
-snakemake -s ${snakefile} --cluster "sbatch" --jobs 100
+snakemake -s ${snakefile} --cluster "sbatch -t 1:00:00 -N 1" --jobs 100
