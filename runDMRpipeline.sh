6d72cf547a837b4a953f62c665b98ffc28f5b616
jildwild <jilke.dewilde@ugent.be>
2021-10-12 12:23:51 +0200

    try without slurm profile

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -4,4 +4,4 @@ ml snakemake/6.1.0-foss-2020b
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 profile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/slurm_profile"
 
-snakemake -s ${snakefile} --profile ${profile} --jobs 100
+snakemake -s ${snakefile} --cluster "sbatch" --jobs 100
