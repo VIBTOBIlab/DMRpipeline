43cd04d3f777454db60dd05633715d8b589141de
jildwild <jilke.dewilde@ugent.be>
2021-10-12 09:32:02 +0200

    load modules

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -1,8 +1,6 @@
 ml purge
 ml snakemake/6.1.0-foss-2020b
 
-export LD_LIBRARY_PATH="/apps/gent/CO7/skylake-ib/software/Python/3.8.6-GCCcore-10.2.0/lib"
-
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 
 snakemake -s ${snakefile} --cluster "qsub" --jobs 100 --rerun-incomplete
