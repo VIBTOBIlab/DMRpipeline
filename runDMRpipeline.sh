450d0c2e221dcffa21bf869ae12ac6ef0262e851
jildwild <jilke.dewilde@ugent.be>
2021-10-13 08:33:34 +0200

    correct resource allocation

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -2,6 +2,5 @@ ml purge
 ml snakemake/6.1.0-foss-2020b
 
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
-profile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/slurm_profile"
 
-snakemake -s ${snakefile} --cluster "sbatch -t 1:00:00 -N 1 -n 4" --jobs 100
+snakemake -s ${snakefile} --cluster "sbatch -t 15:00 -mem 30G" --jobs 100
