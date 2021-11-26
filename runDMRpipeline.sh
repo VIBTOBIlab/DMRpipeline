9b70624268e2e639ac8a081f6046c551a716cefc
jildwild <jilke.dewilde@ugent.be>
2021-11-26 18:41:51 +0100

    more files = more time

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -3,4 +3,4 @@ ml snakemake/6.1.0-foss-2020b
 
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 
-snakemake -s ${snakefile} --cluster "sbatch --time=15:00 --mem=30G" --jobs 100
+snakemake -s ${snakefile} --cluster "sbatch --time=1:00:00 --mem=30G" --jobs 100
