bf69145aa85bad1910de06f2725099f78081733c
jildwild <jilke.dewilde@ugent.be>
2022-04-30 13:54:04 +0200

    time

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -3,4 +3,4 @@ ml snakemake/6.1.0-foss-2020b
 
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 
-snakemake -s ${snakefile} --cluster "sbatch --time=5-0:0 --cpus-per-task=9 --mem=60G" --jobs 100
+snakemake -s ${snakefile} --cluster "sbatch --time=5-00:00:00 --cpus-per-task=9 --mem=60G" --jobs 100
