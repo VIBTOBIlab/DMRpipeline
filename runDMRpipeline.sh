ee5858bf98efede9cdf7bac0632c832a2be7d2f9
jildwild <jilke.dewilde@ugent.be>
2022-05-06 09:54:42 +0200

    unlock

--- runDMRpipeline.sh
+++ runDMRpipeline.sh
@@ -3,4 +3,4 @@ ml snakemake/6.1.0-foss-2020b
 
 snakefile="/data/gent/vo/000/gvo00027/vsc42927/scripts/DMRpipeline/DMRfinder.snakefile"
 
-snakemake -s ${snakefile} --cluster "sbatch --time=4:00:00 --cpus-per-task=11" --jobs 100
+snakemake -s ${snakefile} --cluster "sbatch --time=4:00:00 --cpus-per-task=11" --jobs 100 --unlock
